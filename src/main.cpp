a7e7b01de3289873ca0ee4f44037911d918ca0b3
tiltproofRain <srijannr1@gmail.com>
2020-10-02 12:07:13 +0530

    using namespace tl

--- src/main.cpp
+++ src/main.cpp
@@ -4,28 +4,29 @@
 #include <lexer.hpp>
 #include <utils/str_format.hpp>
 
+using namespace tl;
+
 void lexerTest() {
 	std::string test = "11 + 2 - 3.5 * 4.62";
 	std::string_view name = "<test_script>";
-	tl::Lexer lexer = {name, test};
+	Lexer lexer = {name, test};
 
 	const int expectedCount = 8;
-	using TT = tl::TokenType;
+	using TT = TokenType;
 
-	tl::TokenType expected[expectedCount] = {TT::num, TT::plus, TT::num, TT::minus, TT::num, TT::mult, TT::num, TT::eof_};
+	TokenType expected[expectedCount] = {TT::num, TT::plus, TT::num, TT::minus, TT::num, TT::mult, TT::num, TT::eof_};
 
 	for (int i = 0; i < expectedCount; i++) {
-		tl::Token token = lexer.nextToken();
+		Token token = lexer.nextToken();
 		print("Token: {}\n", token);
 		assert(token.type == expected[i]);
 	}
 }
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char const* argv[]) {
-	using namespace tl;
 	print("--Toylang--\n\n");
-	assert(utils::toStr(tl::TokenType::div) == "div");
-	assert(utils::toEnum("plus", tl::TokenType::error) == tl::TokenType::plus);
+	assert(utils::toStr(TokenType::div) == "div");
+	assert(utils::toEnum("plus", TokenType::error) == TokenType::plus);
 	lexerTest();
 	return 0;
 }
